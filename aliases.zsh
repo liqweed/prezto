547dfc86d0b8bce219443bb48cb700a3cdc992df
Ophir Radnitz <ophir.r@gmail.com>
2014-05-13 23:13:21 +0300

    Added some aliases & env vars

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,19 @@
 alias gw=./gradlew
+alias gwi=./gradlew idea
+
 alias br=brunch
+
+alias bcask="brew cask"
+alias bcasks="brew cask search"
+alias bcaski="brew cask install"
+
+# Docker stuff from: http://www.kartar.net/2014/03/some-useful-docker-bash-functions-and-aliases/
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+# runs a daemonized container
+alias dkd="docker run -d -P"
+# runs an interactive container
+alias dki="docker run -t -i -P"
+
+docker_rm_non_running() { docker rm $(docker ps -q -a); }
+docker_rmove_images() { docker rmi $(docker images -q); }
+db() { docker build -t="$1" .; }
--- env.zsh
+++ env.zsh
@@ -1,1 +1,10 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
+#export JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home"
+
+export DOCKER_HOST=tcp://localhost:4243
+
+export PATH=~/bin:$PATH
+
+export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=256M -XX:MaxPermSize=512M -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9999"
+
+export ANDROID_HOME=/usr/local/opt/android-sdk
