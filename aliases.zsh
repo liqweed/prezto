c4cc7676d9b44fafb121c2cd85f80557a0fa8690
Ophir Radnitz <oradnitz@vmware.com>
2014-12-26 11:57:44 +0200

    local updats

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,9 @@ alias bcask="brew cask"
 alias bcasks="brew cask search"
 alias bcaski="brew cask install"
 
+alias ports="lsof -i -P | grep -i \"listen\""
+alias ports_all="lsof -i -P"
+
 # Docker stuff from: http://www.kartar.net/2014/03/some-useful-docker-bash-functions-and-aliases/
 alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
 # runs a daemonized container
@@ -17,3 +20,8 @@ alias dki="docker run -t -i -P"
 docker_rm_non_running() { docker rm $(docker ps -q -a); }
 docker_rmove_images() { docker rmi $(docker images -q); }
 db() { docker build -t="$1" .; }
+
+jvm() {
+  export JAVA_HOME=`/usr/libexec/java_home -v "$1"`
+  java -version
+}
--- env.zsh
+++ env.zsh
@@ -1,10 +1,19 @@
 #export JAVA_HOME=$(/usr/libexec/java_home)
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home"
 
-export DOCKER_HOST=tcp://localhost:4243
+#export DOCKER_HOST=tcp://192.168.59.103:4243
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/oradnitz/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
 
 export PATH=~/bin:$PATH
 
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=256M -XX:MaxPermSize=512M -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9999"
 
 export ANDROID_HOME=/usr/local/opt/android-sdk
+
+export M2_HOME="/usr/local/Cellar/maven/3.2.3/libexec"
+
+export ATOM_PATH="/opt/homebrew-cask/Caskroom/atom/latest"
+
+export SENCHA_PATH=/Users/oradnitz/workspace/vmware/tools
